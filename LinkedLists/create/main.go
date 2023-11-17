615c6a1ed7bea519acbfb0c396d6f773304facfe
aaochieng <ochiengaoron@gmail.com>
2023-11-17 11:33:54 +0300

    Linked lists practice{ insert,create }

---
+++ LinkedLists/create/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Next *Node
+	Data interface{}
+}
+
+type LinkedList struct {
+	Head *Node
+}
+
+func (l *LinkedList) insert(data interface{}) {
+	newNode := &Node{Data: data, Next: nil}
+
+	if l.Head == nil {
+		l.Head = newNode
+	} else {
+		current := l.Head
+		for current.Next != nil {
+			current = current.Next
+		}
+		current.Next = newNode
+	}
+}
+
+func (l *LinkedList) printList() {
+	for curent := l.Head; curent != nil; curent = curent.Next {
+		fmt.Print(curent.Data, "-->")
+	}
+}
+
+func main() {
+	mylist := LinkedList{}
+	// mylist.insert("Hello World")
+	// mylist.insert("Hello World")
+	// mylist.insert("Hello World")
+	// mylist.insert("Hello World")
+	// mylist.insert("Hello World")
+	mylist.printList()
+}
