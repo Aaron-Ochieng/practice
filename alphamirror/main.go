813a42155bc530b766b15c0fc02e267f83780287
aaochieng <ochiengaoron@gmail.com>
2024-05-23 16:56:16 +0300

    Alpha Mirror

--- alphamirror/main.go
+++ alphamirror/main.go
@@ -2,16 +2,30 @@ package main
 
 import "fmt"
 
+// func AlphaMirror(s string) string {
+// 	str := ""
+
+// 	for _, char := range s {
+// 		if char >= 'A' && char <= 'Z' {
+// 			pos := (char + 25) % 90
+// 			str += string(90 - pos)
+// 		} else if char >= 'a' && char <= 'z' {
+// 			pos := (char + 25) % 122
+// 			str += string(rune(122 - pos))
+// 		} else {
+// 			str += string(char)
+// 		}
+// 	}
+// 	return str
+// }
+
 func AlphaMirror(s string) string {
 	str := ""
-
 	for _, char := range s {
 		if char >= 'A' && char <= 'Z' {
-			pos := (char + 25) % 90
-			str += string(90 - pos)
+			str += string((90-char)%26 + 'A')
 		} else if char >= 'a' && char <= 'z' {
-			pos := (char + 25) % 122
-			str += string(rune(122 - pos))
+			str += string((122-char)%26 + 'a')
 		} else {
 			str += string(char)
 		}
