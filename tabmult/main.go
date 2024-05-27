18407f003f886ba6b0f8e6489c09d63d407e6322
aaochieng <ochiengaoron@gmail.com>
2024-05-27 15:11:50 +0300

    Tabmult

--- tabmult/main.go
+++ tabmult/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"os"
-	"strconv"
+	//"strconv"
+  "github.com/01-edu/z01"
 )
 
-func TabMult(n int) {
-	for i := 1; i < 11; i++ {
-		fmt.Printf("%d"+"x"+"%d"+"="+"%d\n", i, n, (i * n))
-	}
+func atoi(s string)int{
+  res:=0
+  for _,c :=range s{
+    res = res*10 + int(c-'0')
+  }
+  return res
+}
+
+func itoa(n int) string{
+  res:=""
+  if n ==0 {
+    return "0"
+  }
+
+  for n >0{
+    res =string(n%10 + '0') +res
+    n/=10
+  }
+  return res
+}
+
+
+
+func TabMult(s string) {
+  for i:=1;i<=9;i++{
+    str:= itoa(i)+" "+"x"+" "+s+" "+"="+" " + itoa(i*atoi(s))
+
+    for _,char :=range str{
+      z01.PrintRune(char)
+    }
+    z01.PrintRune('\n')
+  }
 }
 
 func main() {
@@ -17,6 +46,5 @@ func main() {
 	if len(args)-1 > 0 || len(args)-1 < 0 {
 		return
 	}
-	y, _ := strconv.Atoi(args[0])
-	TabMult(y)
+	TabMult(args[0])
 }
